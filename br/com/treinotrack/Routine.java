4e68b519f7ba803e77f21fba479cd525611a097b
nertonm <thiago2nerton@gmail.com>
2024-09-21 22:36:41 -0300

    Setting up a list of E'xercise

--- br/com/treinotrack/Routine.java
+++ br/com/treinotrack/Routine.java
@@ -1,5 +1,14 @@
 package br.com.treinotrack;
 
-public class Routine {
+import br.com.treinotrack.User;
+import br.com.treinotrack.Exercise;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Routine {
+	double planId;
+	string name;
+	double duration;
+	List<Exercise> exercises;
+	User assignedTo;
 }
