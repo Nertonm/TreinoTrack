ce5fab76de26252457505e648ada7119113f731b
Luana Teles <142579091+lua-teles@users.noreply.github.com>
2024-09-22 22:31:56 -0300

    Update User.java

--- br/com/treinotrack/User.java
+++ br/com/treinotrack/User.java
@@ -2,42 +2,44 @@ package br.com.treinotrack;
 import br.com.treinotrack.Tracker;
 
 public class User {
-    private String name;
-    private byte age;
-    private float height;
-    private float weight;
-    private float imc;
+    private String name; //nome
+    private byte age; // idade
+    private float height; // altura
+    private float weight; // peso
+    private float imc; // imc
     Tracker userTracker;
     
     public void getIMC(){
         imc = weight / (height * height);
-    }
+    } // faz o imc
 	public String getName() {
 		return name;
-	}
+	} // acessa o nome
 	public void setName(String name) {
 		this.name = name;
-	}
+	} // pega o nome
 	public byte getAge() {
 		return age;
-	}
+	}// acessa a idade
 	public void setAge(byte age) {
 		this.age = age;
-	}
+	} // pega a idade
 	public float getHeight() {
 		return height;
-	}
+	} // acessa a altura
 	public void setHeight(float height) {
 		this.height = height;
-	}
+	} // pega a altura
 	public float getWeight() {
 		return weight;
-	}
+	} // acessa o peso
 	public void setWeight(float weight) {
 		this.weight = weight;
-	}
+	} // pega o peso
 	public float getImc() {
 		return imc;
-	}
+	} // pega o imc
+
+
 }
 
