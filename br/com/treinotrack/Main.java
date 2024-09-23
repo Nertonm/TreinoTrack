2a294543cc64b308e9df340402f9f71734a2c0d8
Luana Teles <142579091+lua-teles@users.noreply.github.com>
2024-09-22 22:31:34 -0300

    Create Main.java

---
+++ br/com/treinotrack/Main.java
@@ -0,0 +1,19 @@
+package br.com.treinotrack;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+        ArrayList<User> users = new ArrayList<User>();
+        Admin ADM = new Admin(users);
+        ADM.CreatNewUser();
+        ADM.ReadUserList();
+
+
+
+
+
+    }
+}
