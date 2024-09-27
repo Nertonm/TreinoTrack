cc76920e4c1f8b0e87a51d01c7a8aecc572997ec
nertonm <thiago2nerton@gmail.com>
2024-09-27 01:13:39 -0300

    New classes for exercises completion

---
+++ br/com/treinotrack/service/Tracker.java
@@ -0,0 +1,11 @@
+package br.com.treinotrack.service;
+
+import br.com.treinotrack.models.Workout;
+
+import java.util.List;
+
+public class Tracker {
+	double trackerId;
+	List<Workout.WorkoutHistory> workoutHistory;
+	
+}
