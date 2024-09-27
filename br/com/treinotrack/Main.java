0765196a96e4d57c8fd88e67851fa5bdee2ff258
nertonm <thiago2nerton@gmail.com>
2024-09-27 17:51:59 -0300

    Altering main

--- br/com/treinotrack/Main.java
+++ br/com/treinotrack/Main.java
@@ -1,23 +1,15 @@
 package br.com.treinotrack;
 import br.com.treinotrack.service.Util;
+import br.com.treinotrack.models.Admin;
 
 public class Main {
     public static void main(String[] args){
-
-
-        /*Scanner scan = new Scanner(System.in);
-        ArrayList<User> users = new ArrayList<User>();
-        Admin ADM = new Admin(users);
-        ADM.CreateNewUser();
+        Admin ADM = new Admin();
         ADM.CreateNewUser();
         ADM.ReadUserList();
         ADM.UpdateUser();
         ADM.ReadUserList();
         ADM.DeleteUser();
         ADM.ReadUserList();
-        */
-
-
-
     }
 }
