2edc173d2a45f274398929d111264a729ce48bff
nertonm <thiago2nerton@gmail.com>
2024-09-28 14:33:15 -0300

    Begging exercise logic

--- br/com/treinotrack/models/exercises/Exercise.java
+++ br/com/treinotrack/models/exercises/Exercise.java
@@ -1,9 +1,25 @@
 package br.com.treinotrack.models.exercises;
 
-public class Exercise {
+abstract class Exercise {
     private String name;
-    private  int reps; // Repetições
-    private int sets;  // Seriest
+    private  int duration;
+    private int caloriesBurned;
+
+    //Builders
+    public Exercise() {
+    	this.name = NULL;
+    	this.duration = 0;
+    	this.caloriesBurned = 0;
+    }
+    public Exercise(String name, int duration, int caloriesBurned) {
+        this.name = name;
+        this.duration = duration;
+        this.caloriesBurned = caloriesBurned;
+    }
+    
+    //Getters and Setters
 }
+
+
 
 
