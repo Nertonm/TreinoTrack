737db86fd1a10973046bbb768b39a8bd60b871b9
Luana Teles <142579091+lua-teles@users.noreply.github.com>
2024-09-29 17:10:42 -0300

    Update Strength.java

--- br/com/treinotrack/models/exercises/Strength.java
+++ br/com/treinotrack/models/exercises/Strength.java
@@ -1,5 +1,24 @@
 package br.com.treinotrack.models.exercises;
 
 
-public class Strength {
+public abstract class Strength extends Exercise{
+    private int series;
+    private int repetiçoes;
+
+    public Strength(String name, int series, int repeticoes){
+        this.setName(name);
+        this.series = series;
+        this.repetiçoes = repeticoes;
+    }
+
+    public int getSeries(){
+        return series;
+    }
+    public int getRepetiçoes(){
+        return repetiçoes;
+    }
+    public void performExercise() {
+        System.out.println("Performing " + getName() + ": " + series + " sets of " + repetiçoes + " repetitions.");
+    }
+
 }
