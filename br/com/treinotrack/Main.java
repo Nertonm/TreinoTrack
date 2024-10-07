5b0a20f344203b00e22c739e37806e5402483949
Luana Teles <142579091+lua-teles@users.noreply.github.com>
2024-10-07 18:10:25 -0300

    Update Main.java

--- br/com/treinotrack/Main.java
+++ br/com/treinotrack/Main.java
@@ -1,15 +1,10 @@
 package br.com.treinotrack;
-import br.com.treinotrack.models.Admin;
+import br.com.treinotrack.facades.AdminFacade;
 
 public class Main {
     public static void main(String[] args){
-        Admin admin = new Admin();
-        admin.CreateNewBob();
-        admin.CreateNewUser();
-        admin.ReadUserList();
-        admin.UpdateUser();
-        admin.ReadUserList();
-        admin.DeleteUser();
-        admin.ReadUserList();
+        AdminFacade facade = new AdminFacade();
+        facade.start(); // Inicia o menu de gerenciamento
+        facade.close(); // Fecha o scanner ao final da execução
     }
 }
