fae074aafe1b369d257e3622093f4b3d3ccf9580
Luana Teles <luanatealves@gmail.com>
2024-11-11 21:13:20 -0300

    Update User.java

--- br/com/treinotrack/models/User.java
+++ br/com/treinotrack/models/User.java
@@ -1,13 +1,14 @@
 package br.com.treinotrack.models;
+import java.io.Serializable;
 import java.util.Scanner;
 
-public class User {
+public class User implements Serializable{
+	private static final long seraialVersionUID = 1L;
     private String name; //nome
     private byte age; // idade em anos
     private float height; // altura em metros
     private float weight; // peso em kg
     private String sex;
-    private float imc; // imc
 
 	// Builder
 	public User(String name, byte age, float height, float weight, String sex){
@@ -16,32 +17,28 @@ public class User {
 		this.height = height;
 		this.weight = weight;
 		this.sex = sex;
-		setIMC();
-	}
-	public User(){
-		this.name = "Bob";
-		this.age = 18;
-		this.height = 1.70f;
-		this.weight = 65.00f;
-		this.sex = "male";
-		setIMC();
 	}
 
 	// Getters and Setters
-	public float getImc() {return imc;} // 
-	public void setIMC() {this.imc = this.weight / (this.height * this.height);}
+	public float getImc() {return weight / (height * height);} //
 
+	//nome
 	public String getName() {return name;}
 	public void setName(String name) {this.name = name;}
 
+	//idade
 	public byte getAge() {return age;}
 	public void setAge(byte age) {this.age = age;} 
 
+	//altura
 	public float getHeight() {return height;}
 	public void setHeight(float height) {this.height = height;}
 
+	//peso
 	public float getWeight() {return weight;} 
 	public void setWeight(float weight) {this.weight = weight;}
+
+	//sexo
 	public void setSex(String sex) {this.sex = sex;}
 
 }
